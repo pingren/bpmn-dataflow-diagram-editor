9a5147187dcccc0bc11339fa401f97ef8aab28cb
Ping <palmerx@qq.com>
2019-08-29 10:49:33 +0800

    feat: pane-left

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,14 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <el-container>
-      <el-aside>
-        <el-card>
-          <div slot="header">
-            <span>第一步：选择数据源</span>
-          </div>
-          <DatabasePicker style="width:300px;" />
-        </el-card>
-
-        <el-card style="height: 50%">
-          <div slot="header">
-            <span>第二步：选择算子</span>
-          </div>
-          <OperatorPicker style="width:300px;" />
-        </el-card>
-      </el-aside>
+      <PaneLeft />
       <BPMNEditor style="height:100vh;width:100%;" />
     </el-container>
   </div>
 </template>
 
 <script>
-import OperatorPicker from './components/OperatorPicker'
-import DatabasePicker from './components/DatabasePicker'
-
+import PaneLeft from './components/PaneLeft'
 import BPMNEditor from './components/BPMNEditor.vue'
 
 // import './app.js'
@@ -33,20 +17,14 @@ import BPMNEditor from './components/BPMNEditor.vue'
 export default {
   name: 'App',
   components: {
-    OperatorPicker,
-    DatabasePicker,
     BPMNEditor,
+    PaneLeft,
   },
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/PaneLeft.vue
@@ -0,0 +1,84 @@
+<template>
+  <el-card
+    class="left-pane"
+    :body-style="{ padding:'0', }"
+  >
+    <el-menu
+      :default-openeds="['1','2']"
+      class="el-menu-vertical-demo"
+      :collapse="isCollapse"
+    >
+      <el-button
+        :icon="isCollapse ? 'el-icon-bottom-right' : 'el-icon-top-left'"
+        size="mini"
+        plain
+        style="border:none;width:100%;"
+        title="折叠/展开"
+        @click="isCollapse = !isCollapse"
+      />
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-coin" />
+          <span slot="title">第一步：数据源
+          </span>
+        </template>
+        <DatabasePicker
+          class="submenu"
+          :style="isCollapse ? 'max-height: 90vh;' : 'max-height: calc(30vh - 35px);'"
+        />
+      </el-submenu>
+      <el-submenu index="2">
+        <template slot="title">
+          <i class="el-icon-magic-stick" />
+          <span slot="title">第二步：选择算子
+          </span>
+        </template>
+        <OperatorPicker
+          class="submenu"
+          :style="isCollapse ? 'max-height: 90vh;' : 'max-height: calc(70vh - 35px);'"
+        />
+      </el-submenu>
+    </el-menu>
+  </el-card>
+</template>
+
+<script>
+import OperatorPicker from './OperatorPicker'
+import DatabasePicker from './DatabasePicker'
+
+export default {
+  name: 'App',
+  components: {
+    OperatorPicker,
+    DatabasePicker,
+  },
+  data() {
+    return {
+      activeNames: ['1', '2'],
+      isCollapse: true,
+    }
+  },
+}
+</script>
+
+<style scoped>
+.left-pane {
+  position: fixed;
+  z-index: 9;
+  left: 0;
+  top: 0;
+  max-height: 100vh;
+}
+/deep/ .el-submenu__title {
+  height: 2.5em;
+  line-height: 2.5em;
+  background-color: #eff5ff;
+  text-align: left;
+}
+/deep/ .el-menu:not(.el-menu--collapse) {
+  width: 300px;
+}
+.submenu {
+  overflow: auto;
+}
+</style>
