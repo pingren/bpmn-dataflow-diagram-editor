69edda5c9d679dd04e5e7e1f7ebcc49e509f6cd2
Ping <palmerx@qq.com>
2020-02-13 11:20:40 +0800

    feat: slider active background

--- src/components/ZoomSlider.vue
+++ src/components/ZoomSlider.vue
@@ -1,5 +1,6 @@
 <template>
   <el-slider
+    class="slider"
     :value="zoomLevel"
     :step="0.001"
     :min="0.2"
@@ -26,76 +27,16 @@ export default {
   },
 }
 </script>
-<style lang="css">
-.devdrawer {
-  height: 100%;
-  overflow: auto !important;
-}
-/* Styles Required */
-@import '~bpmn-js/dist/assets/diagram-js.css';
-/* @import '~bpmn-js/dist/assets/bpmn-font/css/bpmn.css'; */
-/* @import '~bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css'; */
-@import '~bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css';
-
-/* Custom styles for animation */
-.loader {
-  border: 8px solid #f3f3f3; /* Light grey */
-  border-top: 8px solid #3498db; /* Blue */
-  border-radius: 50%;
-  width: 9px;
-  height: 9px;
-  animation: spin 2s linear infinite;
-}
-
-@keyframes spin {
-  0% {
-    transform: rotate(0deg);
+<style lang="scss" scoped>
+.slider {
+  z-index: 1;
+  padding: 10px 0;
+  border-radius: 4px;
+  border: 1px solid transparent;
+  &:active {
+    background: rgba(228, 228, 228, 0.4);
+    border-color: rgba(128, 128, 128, 0.3);
+    box-shadow: 0 2px 12px 0 rgba(0, 0, 0, 0.1);
   }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-.checkmark {
-  display: inline-block;
-  width: 22px;
-  height: 22px;
-  transform: rotate(45deg);
-}
-
-.checkmark_circle {
-  position: absolute;
-  width: 22px;
-  height: 22px;
-  background-color: green;
-  border-radius: 11px;
-  left: 0;
-  top: 0;
-}
-
-.checkmark_stem {
-  position: absolute;
-  width: 3px;
-  height: 9px;
-  background-color: #fff;
-  left: 11px;
-  top: 6px;
-}
-
-.checkmark_kick {
-  position: absolute;
-  width: 3px;
-  height: 3px;
-  background-color: #fff;
-  left: 8px;
-  top: 12px;
-}
-.containers {
-  background-image: linear-gradient(
-      to right,
-      rgba(111, 111, 111, 0.1) 1px,
-      transparent 1px
-    ),
-    linear-gradient(to bottom, rgba(188, 188, 188, 0.1) 1px, transparent 1px);
-  background-size: 50px 50px;
 }
 </style>
